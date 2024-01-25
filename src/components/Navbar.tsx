571ecb8755678496ee3bf70a6789993c4845f13f
nine96as <nine96@skiff.com>
2024-01-25 18:27:02 +0000

    Utilise Icons.logo ref in Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,13 @@
 import Link from 'next/link';
+import { Icons } from './Icons';
 
 const Navbar = () => {
   return (
     <div className='fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2'>
       <div className='container flex items-center justify-between h-full gap-2 mx-auto max-w-7xl'>
-        {/* logo */}
         <Link href='/' className='flex items-center gap-2'>
+          {/* logo */}
+          <Icons.logo className='w-8 h-8 sm:h-6 sm:w-6' />
           <p className='hidden text-sm font-medium text-zinc-700 md:block'>
             Breadit
           </p>
