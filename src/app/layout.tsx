45a4c64a112c1fe2ff0ca32adcfa8247b6a971ad
nine96as <nine96@skiff.com>
2024-01-25 17:54:17 +0000

    Add base styles for Home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,32 @@
-import '@/styles/globals.css'
+import { cn } from '@/lib/utils';
+import '@/styles/globals.css';
+import { GeistSans } from 'geist/font/sans';
 
 export const metadata = {
   title: 'Breadit',
-  description: 'A Reddit clone built with Next.js and TypeScript.',
-}
+  description: 'A Reddit clone built with Next.js and TypeScript.'
+};
 
 export default function RootLayout({
-  children,
+  children
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
-      <body>{children}</body>
+    <html
+      lang='en'
+      className={cn(
+        'bg-white text-slate-900 antialiased light',
+        GeistSans.className
+      )}
+    >
+      <body className={cn('min-h-screen pt-12 bg-slate-50 antialiased')}>
+        {/* <Navbar /> */}
+
+        <div className='container h-full pt-12 mx-auto max-w-7xl'>
+          {children}
+        </div>
+      </body>
     </html>
-  )
+  );
 }
